bb1c03990be8952759e5cd3e3b920f826c7210dd
sahandevs <sahandevs@gmail.com>
2019-02-05 12:53:23 +0330

    Update run.py

--- run.py
+++ run.py
@@ -2,15 +2,19 @@ import json
 from parsers.export_parser import ExportParser
 from interfaces.terminal import TerminalInterface
 from sunit.runner import TestRunner
+import sys
 
 
 def main():
     _raw: str
-    with open(r'tests.json', encoding="utf8") as file:
+    args = sys.argv
+    file = args[1]
+    env = args[2]
+    with open(file, encoding="utf8") as file:
         _raw = file.read()
     parser = ExportParser(json.loads(_raw))
     interface = TerminalInterface()
-    runner = TestRunner(export_parser=parser, environment_name='tests', interface=interface)
+    runner = TestRunner(export_parser=parser, environment_name=env, interface=interface)
     interface.start()
 
 
